52c1a07ccf96ddc315f1809fa7cc2baff4729a00
steve <bashcandicoot@gmail.com>
2018-03-08 22:18:23 +0000

    all people route returns all people successfully

--- server/routes/persona/all.js
+++ server/routes/persona/all.js
@@ -1,6 +1,6 @@
 const db = require('../../models/index');
 
 module.exports = (req, res) => {
-  db.sequelize.models.persona.findAll()
+  db.sequelize.models.Personas.findAll()
     .then(persona => res.status(200).json({ persona }));
 };
