7c5a006b70a8b64805f52d7fa54b28095143d56c
steve <bashcandicoot@gmail.com>
2018-03-08 22:16:39 +0000

    added created_at and updated_at fields to test migration, current_location not working yet

--- server/migrations/insert-persona-data.js
+++ server/migrations/insert-persona-data.js
@@ -1,10 +1,11 @@
 const personaData = [
   {
+    id: '827ccc50-3991-490c-bc4b-2b77a923c5a5',
     job: 'Solicitor',
     company: 'Smith, Haynes and Hooper',
     ssn: 'ZZ376803T',
     residence: '1 Bruce alley\nNew Justin\nL072 TE',
-    current_location: [-66.491849, -69.512524],
+    // current_location: [-66.491849, -69.512524],
     blood_group: 'AB+',
     website: ['https: //www.holmes-saunders.com/',
       'http://foster-ford.com/',
@@ -17,10 +18,12 @@ const personaData = [
     address: '09 Knight parkways\nWest Yvonneshire\nHD23 5NJ',
     mail: 'jshort@hotmail.com',
     birthdate: '1989-07-07',
+    created_at: new Date(),
+    updated_at: new Date(),
   },
 ];
 
 module.exports = {
-  up: queryInterface => queryInterface.bulkInsert('persona', personaData, {}),
-  down: queryInterface => queryInterface.bulkDelete('persona', null, {}),
+  up: queryInterface => queryInterface.bulkInsert('Personas', personaData, {}),
+  down: queryInterface => queryInterface.bulkDelete('Personas', null, {}),
 };
