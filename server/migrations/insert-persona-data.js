83972543db3f6d47dd2ead3bca73404b7839ad8f
steve <bashcandicoot@gmail.com>
2018-03-09 22:14:30 +0000

    insert fake_profiles.json data currently working with no validation

--- server/migrations/insert-persona-data.js
+++ server/migrations/insert-persona-data.js
@@ -1,5 +1,27 @@
 const personaData = require('../fake_profiles.json');
 
+// const personaData = [
+//   {
+//     job: 'Solicitor',
+//     company: 'Smith, Haynes and Hooper',
+//     ssn: 'ZZ376803T',
+//     residence: '1 Bruce alley\nNew Justin\nL072 TE',
+//     current_location: [-66.491849, -69.512524],
+//     blood_group: 'AB+',
+//     website: ['https: //www.holmes-saunders.com/',
+//       'http://foster-ford.com/',
+//       'https://www.farrell-evans.com/',
+//       'http://white-kelly.net/',
+//     ],
+//     username: 'mauriceharris',
+//     name: 'Dr. Mohamed Newton',
+//     sex: 'F',
+//     address: '09 Knight parkways\nWest Yvonneshire\nHD23 5NJ',
+//     mail: 'jshort@hotmail.com',
+//     birthdate: '1989-07-07',
+//   },
+// ];
+
 module.exports = {
   up: queryInterface => queryInterface.bulkInsert('Personas', personaData, {}),
   down: queryInterface => queryInterface.bulkDelete('Personas', null, {}),
