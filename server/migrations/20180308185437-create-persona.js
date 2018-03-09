985641431e27372589505e58f48f10f4248e94f8
steve <bashcandicoot@gmail.com>
2018-03-09 22:13:09 +0000

    removed allowNull for now, auto generate uuid's and created/updated fields

--- server/migrations/20180308185437-create-persona.js
+++ server/migrations/20180308185437-create-persona.js
@@ -1,71 +1,74 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.createTable('Personas', {
-    id: {
-      allowNull: false,
-      primaryKey: true,
-      type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4,
-    },
-    job: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    company: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    ssn: {
-      type: Sequelize.STRING,
-      unique: true,
-    },
-    residence: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    // current_location: {
-    //   type: Sequelize.GEOMETRY('POINT'),
-    //   allowNull: false,
-    // },
-    blood_group: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    website: {
-      type: Sequelize.ARRAY(Sequelize.STRING),
-      allowNull: false,
-    },
-    username: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    sex: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    address: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    mail: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    birthdate: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    created_at: {
-      allowNull: false,
-      type: Sequelize.DATE,
-    },
-    updated_at: {
-      allowNull: false,
-      type: Sequelize.DATE,
-    },
-  }),
+  up: (queryInterface, Sequelize) => queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+    .then(() => queryInterface.createTable('Personas', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal('uuid_generate_v4()'),
+        primaryKey: true,
+        // allowNull: false,
+      },
+      job: {
+        type: Sequelize.STRING,
+        // allowNull: false,
+      },
+      company: {
+        type: Sequelize.STRING,
+        // allowNull: false,
+      },
+      ssn: {
+        type: Sequelize.STRING,
+        // unique: true,
+      },
+      residence: {
+        type: Sequelize.STRING,
+        // allowNull: false,
+      },
+      current_location: {
+        type: Sequelize.ARRAY(Sequelize.DOUBLE),
+        // allowNull: false,
+      },
+      blood_group: {
+        type: Sequelize.STRING,
+        // allowNull: false,
+      },
+      website: {
+        type: Sequelize.ARRAY(Sequelize.STRING),
+        // allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        // allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        // allowNull: false,
+      },
+      sex: {
+        type: Sequelize.STRING,
+        // allowNull: false,
+      },
+      address: {
+        type: Sequelize.STRING,
+        // allowNull: false,
+      },
+      mail: {
+        type: Sequelize.STRING,
+        // allowNull: false,
+      },
+      birthdate: {
+        type: Sequelize.STRING,
+        // allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+        // allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+        // allowNull: false,
+      },
+    })),
   down: queryInterface => queryInterface.dropTable('Personas'),
 };
