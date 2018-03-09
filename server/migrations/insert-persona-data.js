3ead7935ca1e1b8a0ebf9ef545479dd43d290bff
steve <bashcandicoot@gmail.com>
2018-03-09 17:47:09 +0000

    use fake_orofile.json data instead of a snippet

--- server/migrations/insert-persona-data.js
+++ server/migrations/insert-persona-data.js
@@ -1,27 +1,4 @@
-const personaData = [
-  {
-    id: '827ccc50-3991-490c-bc4b-2b77a923c5a5',
-    job: 'Solicitor',
-    company: 'Smith, Haynes and Hooper',
-    ssn: 'ZZ376803T',
-    residence: '1 Bruce alley\nNew Justin\nL072 TE',
-    // current_location: [-66.491849, -69.512524],
-    blood_group: 'AB+',
-    website: ['https: //www.holmes-saunders.com/',
-      'http://foster-ford.com/',
-      'https://www.farrell-evans.com/',
-      'http://white-kelly.net/',
-    ],
-    username: 'mauriceharris',
-    name: 'Dr. Mohamed Newton',
-    sex: 'F',
-    address: '09 Knight parkways\nWest Yvonneshire\nHD23 5NJ',
-    mail: 'jshort@hotmail.com',
-    birthdate: '1989-07-07',
-    created_at: new Date(),
-    updated_at: new Date(),
-  },
-];
+const personaData = require('../fake_profiles.json');
 
 module.exports = {
   up: queryInterface => queryInterface.bulkInsert('Personas', personaData, {}),
