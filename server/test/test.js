c5fc03c6384280a07bcaab5708cc10259bf9ee99
steve <bashcandicoot@gmail.com>
2018-03-11 19:56:22 +0000

    test people route added

--- server/test/test.js
+++ server/test/test.js
@@ -1,9 +1,20 @@
-const assert = require('assert');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../src/app');
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal(-1, [1, 2, 3].indexOf(4));
-    });
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('/GET people', () => {
+  it('it should GET all the people', (done) => {
+    chai.request(app)
+      .get('/people')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('array');
+        // res.body.length.should.be.eql(0);
+        done();
+      });
   });
 });
+
