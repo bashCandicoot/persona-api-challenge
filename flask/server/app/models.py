12ddcca1042c525ce17c3ea5bab6549c68f9dcd8
steve <bashcandicoot@gmail.com>
2018-03-13 21:28:23 +0000

    init persona model

---
+++ flask/server/app/models.py
@@ -0,0 +1,44 @@
+from app import db
+
+
+class Persona(db.Model):
+
+    __tablename__ = 'personas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    job = db.Column(db.String(100))
+    company = db.Column(db.String(100))
+    ssn = db.Column(db.String(9))
+    residence = db.Column(db.String(100))
+    current_location = db.Column(db.ARRAY(float))
+    blood_group = db.Column(db.String(100))
+    website = db.Column(db.ARRAY(str))
+    username = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+    sex = db.Column(db.String(1))
+    address = db.Column(db.String(100))
+    mail = db.Column(db.String(100))
+    birthdate = db.Column(db.String(10))
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
+
+    def __init__(self, name):
+        """initialize with name."""
+        self.name = name
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return Persona.query.all()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "<Persona: {}>".format(self.name)
