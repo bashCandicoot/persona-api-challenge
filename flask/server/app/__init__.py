8cb51b4b9769ec0e9382cbab1cca23a60a640d1e
steve <bashcandicoot@gmail.com>
2018-03-14 22:22:56 +0000

    add people route, needs pagination

--- flask/server/app/__init__.py
+++ flask/server/app/__init__.py
@@ -1,16 +1,46 @@
 
+from flask import abort, jsonify, request
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
-
 from instance.config import app_config
 
 db = SQLAlchemy()
 
 
 def create_app(config_name):
+    from app.models import Persona
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+
+    @app.route('/people/', methods=['GET'])
+    def people():
+        people = Persona.get_all()
+        results = []
+        for person in people:
+            obj = {
+                'id': person.id,
+                'job': person.job,
+                'company': person.company,
+                'ssn': person.ssn,
+                'residence': person.residence,
+                'current_location': person.current_location,
+                'blood_group': person.blood_group,
+                'website': person.website,
+                'username': person.username,
+                'name': person.name,
+                'sex': person.sex,
+                'address': person.address,
+                'mail': person.mail,
+                'birthdate': person.birthdate,
+                'date_created': person.date_created,
+                'date_modified': person.date_modified
+            }
+            results.append(obj)
+        response = jsonify(results)
+        response.status_code = 200
+        return response
+
     return app
