e09e31dd8cd7bf36e0bced76c70ca81d4ca6c18a
steve <bashcandicoot@gmail.com>
2018-03-14 22:24:04 +0000

    modfied field lengths, added default dates for date_created and date_modified

--- flask/server/app/models.py
+++ flask/server/app/models.py
@@ -8,9 +8,9 @@ class Persona(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     job = db.Column(db.String(100))
     company = db.Column(db.String(100))
-    ssn = db.Column(db.String(9))
+    ssn = db.Column(db.String(15))
     residence = db.Column(db.String(100))
-    current_location = db.Column(db.ARRAY(db.FLOAT))
+    current_location = db.Column(db.ARRAY(db.String))
     blood_group = db.Column(db.String(100))
     website = db.Column(db.ARRAY(db.String))
     username = db.Column(db.String(100))
@@ -18,14 +18,11 @@ class Persona(db.Model):
     sex = db.Column(db.String(1))
     address = db.Column(db.String(100))
     mail = db.Column(db.String(100))
-    birthdate = db.Column(db.String(10))
+    birthdate = db.Column(db.String(20))
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(
-        db.DateTime, default=db.func.current_timestamp(),
-        onupdate=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
     def __init__(self, name):
-        """initialize with name."""
         self.name = name
 
     def save(self):
